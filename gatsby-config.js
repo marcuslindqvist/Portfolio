2f884894f4e35be7e5a5220e0da07ec21a177146
marcuslindqvist <marcus.lindqvist@iths.se>
2021-11-26 09:42:55 +0100

    gitignore update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,37 @@
+require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Marcus Gatsby",
-  },
-  plugins: [],
+    siteMetadata: {
+        siteUrl: "https://www.yourdomain.tld",
+        title: "Marcus Gatsby",
+    },
+    plugins: [
+        {
+            resolve: `gatsby-source-contentful`,
+            options: {
+                spaceId: `8zae7nvz50pi`,
+                // Learn about environment variables: https://gatsby.dev/env-vars
+                accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-sharp`,
+            options: {
+                // Defaults used for gatsbyImageData and StaticImage
+                defaults: {},
+                // Set to false to allow builds to continue on image errors
+                failOnError: true,
+                // deprecated options and their defaults:
+                base64Width: 20,
+                forceBase64Format: `jpg`, // valid formats: png,jpg,webp
+                useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+                stripMetadata: true,
+                defaultQuality: 50,
+            },
+        },
+        `gatsby-plugin-image`,
+        `gatsby-plugin-sharp`,
+    ],
 };
